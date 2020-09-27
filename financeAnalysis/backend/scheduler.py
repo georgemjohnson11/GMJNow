c558b212248d2d0cd98f56fed847e213cc9a2e77
georgemjohnson11 <georgemjohnson11@gmail.com>
2020-09-27 09:50:24 -0600

    Move job scheduling tasks to automation and remove cron package Upgrade Django to 3.1.2 from 2.2

--- financeAnalysis/backend/scheduler.py
+++
@@ -1,9 +0,0 @@
-# Look into Airflow instead https://www.saturncloud.io/s/job-scheduling-with-python/
-
-from crontab import CronTab
-
-cron = CronTab(user='root')
-job = cron.new(command='source /app/venv/bin/activate && python get_stocks.py')
-job.minute.on(0)
-job.hour.on(21) #UTC Time when stock market closes
-cron.write()
