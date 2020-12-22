19155970f237bbe0023659e250130e8c51dc80bc
George Johnson <georgemjohnson11@gmail.com>
2020-12-21 17:24:38 -0700

    Fix blog urls Update static assets to host via CDN

--- Blog/urls.py
+++ Blog/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'blog/', include('blog.urls'))
 """
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from websiteBackbone import views
 
@@ -27,3 +29,6 @@ urlpatterns = [
     path(r'positions/', include('positions.urls')),
     path(r'', include('websiteBackbone.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
